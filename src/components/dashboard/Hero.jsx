f4cce1237509ff2b91eccd450a824c1873172ee5
ahmed0-17 <malikalihyd123@gmail.com>
2026-08-20 00:19:01 +0500

    resert hero color

--- src/components/dashboard/Hero.jsx
+++ src/components/dashboard/Hero.jsx
@@ -170,7 +170,7 @@ function Hero() {
 
             <h1 className="text-3xl font-black tracking-tight text-foreground sm:text-5xl">
               {greeting.title},{" "}
-              <span className="text-indigo-500">
+              <span>
                 {userName}!
               </span>
             </h1>
